2499594a2db54666207082417eac32318b536818
Matias Godoy <79279761+matigodoy@users.noreply.github.com>
2024-07-29 19:04:23 -0300

    better debug, update .gitignore and remove unnecessary files

--- src/main/java/compiladores/While.java
+++ src/main/java/compiladores/While.java
@@ -9,7 +9,6 @@ public class While {
     public String generateIntermediateCode(compiladoresParser.WhileContext ctx, Stack<String> tempStack) {
         StringBuilder code = new StringBuilder();
 
-
         // Generar una nueva etiqueta para el inicio del bucle
         String startLabel = getNewLabel();
         String endLabel = getNewLabel();
@@ -22,6 +21,7 @@ public class While {
 
         // Añadir instrucción de salto condicional
         String tempVar = tempStack.pop();
+
         code.append("if ").append(tempVar).append(" njmp ").append(endLabel).append("\n");
 
         // Agregar lineas de codigo que se encuentran en el bloque if
@@ -41,20 +41,16 @@ public class While {
 
     private String generateIntermediateCode(compiladoresParser.CondicionContext ctx, Stack<String> tempStack) {
         StringBuilder code = new StringBuilder();
-        String tempVar = getNewTempVar();
         code.append(generateIntermediateCode(ctx.comparacion(), tempStack));
-        code.append(tempVar).append(" = ").append(tempStack.pop()).append("\n");
-        tempStack.push(tempVar);
-
         return code.toString();
     }
-
 
     private String generateIntermediateCode(compiladoresParser.ComparacionContext ctx, Stack<String> tempStack) {
         StringBuilder code = new StringBuilder();
         String tempVar = getNewTempVar();
         tempStack.push(tempVar);
-        code.append(tempVar).append(" = ").append(ctx.getChild(0).getText()).append(" ").append(ctx.getChild(1).getText()).append(" ").append(ctx.getChild(2).getText()).append("\n");
+        code.append(tempVar).append(" = ").append(ctx.getChild(0).getText()).append(" ")
+                .append(ctx.getChild(1).getText()).append(" ").append(ctx.getChild(2).getText()).append("\n");
         return code.toString();
     }
 
